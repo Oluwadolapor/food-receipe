241311ce940190d0f211e6b3c8ba7fa440522ead
Onifade Dolapo <hardolipey@gmail.com>
2020-10-16 10:37:19 +0100

    Updates

---
+++ precache-manifest.8228c79a0adbbe70b87967fa4803c484.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb9a81950f718b7e5392971796fe909",
+    "url": "/food-receipe/index.html"
+  },
+  {
+    "revision": "e7530f1c7f93b6233407",
+    "url": "/food-receipe/static/css/main.ac3b06c2.chunk.css"
+  },
+  {
+    "revision": "741feba587c4ac59141a",
+    "url": "/food-receipe/static/js/2.d286aa98.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/food-receipe/static/js/2.d286aa98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7530f1c7f93b6233407",
+    "url": "/food-receipe/static/js/main.58f2ed5d.chunk.js"
+  },
+  {
+    "revision": "8bd151779d628c5a31ce",
+    "url": "/food-receipe/static/js/runtime-main.f5335df0.js"
+  }
+]);
